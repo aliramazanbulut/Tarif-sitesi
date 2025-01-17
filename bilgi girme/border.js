dc0ad88934f8c30ad4cbdadd9d50170451e2ff7d
aliramazanbulut <146792031+aliramazanbulut@users.noreply.github.com>
2025-01-17 14:13:41 +0300

    Add files via upload

---
+++ bilgi girme/border.js
@@ -0,0 +1,9 @@
+let a = 0 ;
+localStorage.setItem('tutucu', JSON.stringify(a));
+localStorage.setItem('kullanici', JSON.stringify(a));
+localStorage.setItem('durum', Number.JSON.stringify(a));
+let b =[];
+localStorage.setItem('aramametin', JSON.stringify(a))
+localStorage.setItem('tarifmetin', JSON.stringify(a))
+localStorage.setItem('tarifler', JSON.stringify(a))
+localStorage.setItem('users', JSON.stringify(a))
